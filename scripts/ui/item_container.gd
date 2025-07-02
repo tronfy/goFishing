83d539831ed6cf7f0d30ab58b06a2f63c3789a17
Nícolas D. Schmidt <nicolasdschmidt@gmail.com>
2025-07-01 22:13:06 -0300

    exibir sprites na mochila

--- scripts/ui/item_container.gd
+++ scripts/ui/item_container.gd
@@ -4,6 +4,13 @@ extends Button
 static var scene := preload("res://scenes/item.tscn")
 var s: TelaItem
 
+@onready var icones = {
+	3: preload("res://sprites/fishing_game_assets/4 Icons/Icons_20.png"),
+	4: preload("res://sprites/fishing_game_assets/4 Icons/Icons_11.png"),
+	5: preload("res://sprites/fishing_game_assets/4 Icons/Icons_12.png"),
+	6: preload("res://sprites/fishing_game_assets/4 Icons/Icons_14.png")
+}
+
 var id: int
 var nome: String
 var descricao: String
@@ -19,6 +26,10 @@ func with_data(_id: int, _nome: String, _descricao: String, _qtd: int) -> ItemCo
 func _ready() -> void:
 	$ItemLabel.text = nome
 	$QuantidadeLabel.text = str(qtd) + "x"
+	
+	var showIcone = icones.get(id)
+	if showIcone:
+		$Texture.texture = showIcone
 
 func _on_pressed() -> void:
 	s = scene.instantiate().with_data(id, nome, descricao, qtd)
